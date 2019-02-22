b83cdfd6faca13a2e66673bf1b9bc1b3e2d64a09
Tyler Sell <tylerselldesign@gmail.com>
2019-02-22 22:13:00 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190222214556) do
+ActiveRecord::Schema.define(version: 20190222220853) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password"
+    t.text   "password_digest"
   end
 
 end
