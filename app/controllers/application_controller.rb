17e2d5d76ff61033b4dd5e11bedd24f22ce229e0
Tyler Sell <tylerselldesign@gmail.com>
2019-02-22 21:49:17 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,8 +17,12 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    #your code here
-
+    user = User.new(:username => params[:username], :password => params[:password])
+    
+    if 
+      
+    else 
+      redirect to 
   end
 
   get '/account' do
