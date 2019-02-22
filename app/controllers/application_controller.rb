2f29186c0b290b1372c74ce3c14be35eb5c57c46
Tyler Sell <tylerselldesign@gmail.com>
2019-02-22 21:57:08 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,6 +17,8 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
+    if params[:username].empty?
+    
     user = User.new(:username => params[:username], :password => params[:password])
     
     if user.save
